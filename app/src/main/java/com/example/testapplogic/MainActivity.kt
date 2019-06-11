43d5e73c9e6cf61445e3d49733d23415270a5b28
sunsosay <5801@cdg.co.th>
2019-06-11 16:15:05 +0700

    commit main and layout

--- app/src/main/java/com/example/testapplogic/MainActivity.kt
+++ app/src/main/java/com/example/testapplogic/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.testapplogic
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testapplogic.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
+    var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        init()
+    }
+    private fun init(){
+        binding?.apply {
+            tv1.text = "x"
+        }
     }
 }
